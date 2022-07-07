54bcb9b262cc3051bbd282d04fb1edf710f4f4a6
pauloandrecss <pauloandrecss@gmail.com>
2022-07-06 22:13:22 -0300

    Exercicio resolvido

--- src/javaBasico/Exercicio02.java
+++ src/javaBasico/Exercicio02.java
@@ -5,36 +5,20 @@ import java.util.Scanner;
 public class Exercicio02 {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
-        int resultado = 0;
-        boolean valido = true;
         System.out.println("Digite um numero.");
         int a = entrada.nextInt();
         System.out.println("Digite outro valor.");
         int b = entrada.nextInt();
-        System.out.println("Escolha qual operação matematica a ser feita, digite : \n 1 para adição \n 2 para subtração \n 3 para multiplicação \n 4 para divisão");
-        byte escolha = entrada.nextByte();
+        System.out.println("A soma do valor " + a + " e o valor " + b + " é igual a: " + (a + b) +
+                "\n A subtração do valor " + a + " pelo valor " + b + " é igual a: " + (a - b) +
+                "\nA multiplicação do valor " + a + " com o valor " + b + " é igual a: " + (a * b) +
+                "\nA divisão do valor " + a + " pelo valor " + b + " é igual a: " + (a / b));
 
-        switch (escolha) {
-            case 1:
-            resultado = a + b;
-            break;
-            case 2:
-            resultado = a - b;
-            break;
-            case 3:
-            resultado = a * b;
-            break;
-            case 4:
-            resultado = a / b;
-            break;
-            default:
-                valido = false;
-                System.out.println("Erro: Numero invalido digitado");
-            break;
+
+
 
         }
-        if (valido == true) {
-            System.out.println("O resultado da conta é igual a: " + resultado);
+
         }
-    }
-}
+
+
